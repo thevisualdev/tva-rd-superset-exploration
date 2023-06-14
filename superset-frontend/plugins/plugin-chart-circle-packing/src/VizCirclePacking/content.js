5ff854c64cb1868f2b99935c0efff18763c10309
Erlemorgaine <erle_3@hotmail.com>
2023-06-14 18:29:36 +0200

    graph works

---
+++ superset-frontend/plugins/plugin-chart-circle-packing/src/VizCirclePacking/content.js
@@ -0,0 +1,13 @@
+export const content = {
+  legend: {
+    title: "How to read it",
+    labels: {
+      total: "Total no. of users",
+      active: "No. of active users",
+    },
+  },
+  tooltip: {
+    usersPercentage: "Users (%)",
+    usersNr: "Users (No.)",
+  },
+};
